519e39fde563a2f6b8aa14bb64b5f15cec8d837f
PETITGENET AMBROISE <apetitgenet@adgroupe.priv>
2019-08-09 21:31:48 +0200

    merge

--- Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
+++ Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -34,43 +33,6 @@ namespace Tests
         {
             const int PAGE = 0; // take the first page of results
             const int MAX_RESULTS = 10; // sample up to 10 results
-=======
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using TailSpin.SpaceGame.Web;
-using TailSpin.SpaceGame.Web.Models;
-
-namespace Tests
-{
-    public class DocumentDBRepository_GetItemsAsyncShould
-    {
-        private IDocumentDBRepository<Score> _scoreRepository;
-
-        [SetUp]
-        public void Setup()
-        {
-            using (Stream scoresData = typeof(IDocumentDBRepository<Score>)
-                .Assembly
-                .GetManifestResourceStream("Tailspin.SpaceGame.Web.SampleData.scores.json"))
-            {
-                _scoreRepository = new LocalDocumentDBRepository<Score>(scoresData);
-            }
-        }
-
-        [TestCase("Milky Way")]
-        [TestCase("Andromeda")]
-        [TestCase("Pinwheel")]
-        [TestCase("NGC 1300")]
-        [TestCase("Messier 82")]
-        public void FetchOnlyRequestedGameRegion(string gameRegion)
-        {
-            const int PAGE = 0; // take the first page of results
-            const int MAX_RESULTS = 10; // sample up to 10 results
->>>>>>> master
 
             // Form the query predicate.
             // This expression selects all scores for the provided game region.
@@ -81,7 +43,6 @@ namespace Tests
                 queryPredicate, // the predicate defined above
                 score => 1, // we don't care about the order
                 PAGE,
-<<<<<<< HEAD
                 MAX_RESULTS
             );
             IEnumerable<Score> scores = scoresTask.Result;
@@ -90,14 +51,4 @@ namespace Tests
             Assert.That(scores, Is.All.Matches<Score>(score => score.GameRegion == gameRegion));
         }
     }
-=======
-                MAX_RESULTS
-            );
-            IEnumerable<Score> scores = scoresTask.Result;
-
-            // Verify that each score's game region matches the provided game region.
-            Assert.That(scores, Is.All.Matches<Score>(score => score.GameRegion == gameRegion));
-        }
-    }
->>>>>>> master
 }
